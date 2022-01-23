70d149aab49d7d1c16fc5ffe97b2319ad51192c1
Oni <b1tchslayer2005@gmail.com>
2022-01-23 22:24:56 +0300

    created

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const authRouter = require('./authRoutes')
+const bodyParser = require('body-parser')
+const User = require('./models/User')
+const PORT = process.env.PORT || 3000
+//let CONTACTS = [
+//    {name: 'str', value: 'str1'}
+//]
+
+app.use(express.json())
+app.use("/auth", authRouter)
+app.use('/', express.static(path.join(__dirname, 'client')))
+app.use(bodyParser.json())
+
+
+
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body
+    const user = await User.findOne({ username, password }).lean()
+
+    if (!user) {
+        return res.json({status: 'error', error: 'Ты ввел неверные данные'})
+    }
+
+    res.json({status: 'Ты ввел правильные данные', message : 'Иди нахуй'})
+})
+
+
+// app.post('/api/login', (req, res) => {
+//    // Insert Login Code Here
+//    let username = req.body.username;
+//    let password = req.body.password;
+//    res.send(`poshel nah`);
+//});
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+})
+
+
+
+
+const start = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://newnew:newpassword@cluster0.sixke.mongodb.net/newnew?retryWrites=true&w=majority`)
+
+
+
+
+        app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+start()
